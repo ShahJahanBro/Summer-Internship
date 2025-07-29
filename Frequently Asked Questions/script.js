408fdfde80c2d2ac2e46f59aa5af4c4bc2d3e5b4
ShahJahanBro <shahjahan12098@gmail.com>
2025-07-29 14:35:39 +0500

    JS for question answer toggling

--- Frequently Asked Questions/script.js
+++ Frequently Asked Questions/script.js
@@ -20,3 +20,21 @@ window.addEventListener('load', ()=> {
   title.style.display = "flex"
   title.style.justifyContent = "center"
 });
+
+
+const questionContainers = document.querySelectorAll(".question_container");
+
+questionContainers.forEach((container) => {
+  const question = container.querySelector(".question");
+
+  question.addEventListener("click", () => {
+    questionContainers.forEach((item) => {
+      if (item !== container) {
+        item.classList.remove("active");
+      }
+    });
+
+    
+    container.classList.toggle("active");
+  });
+});
